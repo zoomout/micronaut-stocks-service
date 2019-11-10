0e81d29ea026181bb65fc1123ce32c3468cb268a
zoomout <bzastavnyi@gmail.com>
2019-11-10 06:06:27 +0100

    pagination instead of fixed values

--- src/main/java/com/bogdan/web/StocksListViewController.java
+++ src/main/java/com/bogdan/web/StocksListViewController.java
@@ -3,12 +3,14 @@ package com.bogdan.web;
 import com.bogdan.dto.StockDto;
 import com.bogdan.service.StocksService;
 import io.micronaut.core.util.CollectionUtils;
+import io.micronaut.data.model.Pageable;
 import io.micronaut.http.HttpResponse;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
 import io.micronaut.views.View;
 
 import javax.inject.Inject;
+import javax.validation.Valid;
 import java.util.List;
 
 @Controller
@@ -19,8 +21,8 @@ class StocksListViewController {
 
   @View("stocksList")
   @Get()
-  public HttpResponse listStocks() {
-    List<StockDto> stocks = stocksService.getStocks(0, 20); //TODO implement pagination
+  public HttpResponse listStocks(@Valid Pageable pageable) {
+    List<StockDto> stocks = stocksService.getStocks(pageable.getOffset(), pageable.getSize());
     return HttpResponse.ok(CollectionUtils.mapOf(
         "amount", 5,
         "stocks", stocks
