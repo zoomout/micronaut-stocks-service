58bbdbebe760c5a74e3883159d35467ac9f0e3ea
zoomout <bzastavnyi@gmail.com>
2019-11-10 22:03:52 +0100

    added logger for error

--- src/main/java/com/bogdan/exception/EntityNotFoundExceptionHandler.java
+++ src/main/java/com/bogdan/exception/EntityNotFoundExceptionHandler.java
@@ -7,6 +7,8 @@ import io.micronaut.http.annotation.Produces;
 import io.micronaut.http.hateoas.JsonError;
 import io.micronaut.http.hateoas.Link;
 import io.micronaut.http.server.exceptions.ExceptionHandler;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.inject.Singleton;
 
@@ -15,8 +17,11 @@ import javax.inject.Singleton;
 @Requires(classes = {EntityNotFoundException.class, ExceptionHandler.class})
 public class EntityNotFoundExceptionHandler implements ExceptionHandler<EntityNotFoundException, HttpResponse> {
 
+  private static final Logger LOG = LoggerFactory.getLogger(EntityNotFoundExceptionHandler.class);
+
   @Override
   public HttpResponse<JsonError> handle(HttpRequest request, EntityNotFoundException exception) {
+    LOG.error(exception.getLocalizedMessage(), exception);
     JsonError error = new JsonError(exception.getMessage()).link(Link.SELF, Link.of(request.getUri()));
     return HttpResponse.<JsonError>badRequest().body(error);
   }
